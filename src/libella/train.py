c128eb5c5de30300c504067007a2a3a728f24cd5
Ash <hendoone2014@gmail.com>
2026-08-25 22:05:56 +0000

    Aug 26, 2026, 1:35 AM

--- src/libella/train.py
+++ src/libella/train.py
@@ -531,8 +531,6 @@ def _train_loop(
                 train_chunk_count += 1
                 if len(src) > 0:
                     del core_mask, edge_mask
-                if len(src_loss) > 0 and delta_h is not None and spatial_progress > 0.0:
-                    del edge_bio_cos, bio_dist, hard_neg_mask, hard_neg_src, hard_neg_dst, pos_src, pos_dst
                 del train_idx, x_train, recon_train, z_train, aux_recon_train, r_norm_train, k_i_train, routed_scores_train, base_sae_loss, base_align_val, true_batch_loss, src_loss, dst_loss, A_ij_loss, l_spatial_rel, spatial_loss_val
 
                 # Validation Evaluation
